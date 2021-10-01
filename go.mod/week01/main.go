14255595ede044fca71f65e008111709778c46e2
JT224 <84460206+JT224@users.noreply.github.com>
2021-10-01 14:46:20 -0400

    Create main.go

---
+++ go.mod/week01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var word string = "Go"
+var num int = 42
+var TF bool = true
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println()
+	fmt.Println("Printing", word)
+	fmt.Println("Printing", num)
+	fmt.Println("Printing", TF)
+}
