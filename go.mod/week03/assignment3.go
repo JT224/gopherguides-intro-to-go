c7223e684a92a21b7d26287f2a7e44b3904a104a
JT224 <84460206+JT224@users.noreply.github.com>
2021-10-17 23:32:30 -0400

    Update assignment3.go
    Included in this Pull Request is a framework for creating a “Movie” struct with two public fields, “Length” as an int and “Name” as a string. This program also defines several methods on the pointer receiver for Movie including “Rate” and “Play. Along with methods on pointer receivers, there are methods on value receivers for Movie named “Viewers”, “Plays”, “Rating”, and “String”. These pointer receivers and value receivers are not complete, but is a working framework. Another struct, “Theatre”, was defined without any public fields. Theatre has an accompanying framework for a pointer receiver, “Play”, and a value receiver, “Critique”. 
    	The change from the original program can be seen above. This is an original program and the first pull request being made. There are no previous versions of this program to compare these changes to. More changes to this project are imminent and a new pull request will be submitted to reflect further changes made. 
    	I ran into several challenges while trying to this program together. I did not have nearly as much time to prepare for this program as I did the previous one, but I am prouder of what I was able to discover. I am still currently stuck on the variables I will need to create for the pointer receivers and value receivers. I am also working on creating a counter for the “Rate” section to keep track of how many times Rate is called. The largest hurdle that threw me off was the float32 in the beginning. I believe if I did not spend so much time attempting to figure that part out, I may have started brainstorming on the whole picture first. I missed the “defining a new type named CritiqueFn” in its entirety as I was more focused on some of the more repetitive actions so I struggled with the creation of a new type. I believe these concepts will become clearer to me as the program progresses and as I have time to truly analyze the programs. 
    	I was surprised that this chapter was all about functions as last week seemed to introduce them first. This week definitely was more in depth with functions, but I wish we would have completed this week before last week as I believe it would have made last week’s program with functions easier to comprehend. I was also surprised by how little of a difference there is between a pointer receiver and a value receiver. I am still unsure of why one would use a value receiver over a pointer receiver, but I fully intend on looking into that when I have additional time to do so.

--- go.mod/week03/assignment3.go
+++ go.mod/week03/assignment3.go
@@ -1,1 +1,57 @@
-Test
+package main
+
+import (
+	"fmt"
+)	
+
+
+type Movie struct {
+	Length  int
+	Name string
+}
+func (m *Movie) Rate(float32) {
+//	if 
+//	m.counter += 1
+//m.counter = 0
+	
+}
+
+func (m *Movie) Play(viewers int){
+	return m.Playings 
+}
+
+func (m Movie) Viewers() int{
+	return //return int # of people who have viewed movie
+}
+
+func (m Movie) Plays() int{
+	return //return int # of times the movie has been played. total ratings divided by times movie is played
+}
+
+func (m Movie) Rating() float64{
+	return //returns the rating float64 of the movie. 
+}
+
+func (m Movie) String() string{
+	return Name, Length, Rate //returns name, length, and rating
+}
+
+type Theatre struct {
+
+}
+
+func (t *Theatre) Play(int){
+
+}
+
+func (t Theatre) Crtique := []string{"John","Paul","George","Ringo"{
+
+}
+func main() {
+	a_Movie := Movie{6, "Endgame"}
+
+	fmt.Println(a_Movie)
+	fmt.Println(Length)
+	fmt.Println("Success!")
+
+}
